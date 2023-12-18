63584f0dcf748fdf03ac35ed71afe625589b0e73
Ramon Niet <Ramon.Niet@tu-dortmund.de>
2023-12-18 13:59:15 +0100

    remove unneeded stuff

--- aliases/.aliases.sh
+++ aliases/.aliases.sh
@@ -1,31 +1,6 @@
-alias mountbam='sshfs bam:/home/rniet $HOME/mounts/bam -F ~/.ssh/config -o auto_cache,reconnect,follow_symlinks,volname=bam'
-alias mountbam%e5='sshfs bam%e5:/home/rniet $HOME/mounts/bam -F ~/.ssh/config -o auto_cache,reconnect,follow_symlinks,volname=bam'
-
-function setROOT()
-{
-  export ROOTSYS=/afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.21/x86_64-slc6-gcc48-opt/root
-  export PATH=$ROOTSYS/bin:$PATH
-  export LD_LIBRARY_PATH=$ROOTSYS/lib:/usr/local/lib:$LD_LIBRARY_PATH
-  export BOOST_ROOT=/cvmfs/lhcb.cern.ch/lib/lcg/releases/LCG_71/Boost/1.55.0_python2.7/x86_64-slc6-gcc48-opt
-  export BOOSTROOT=/cvmfs/lhcb.cern.ch/lib/lcg/releases/LCG_71/Boost/1.55.0_python2.7/x86_64-slc6-gcc48-opt
-  export LD_LIBRARY_PATH=$BOOST_ROOT/lib:$LD_LIBRARY_PATH
-  export ROOTVERS=5.34.21
-  export PYTHONPATH=$ROOTSYS/lib:$PYTHONPATH
-}
-
-function py3root6Setup()
-{
-  cd /opt/rh/miniconda/envs/py3root6
-  source /opt/rh/miniconda/scripts/py3root6.sh
-  cd -
-
-  export DOOFITSYS=/doosoft/doofit/ROOT_v6.06.00/
-  export DOOCORESYS=/doosoft/doocore/ROOT_v6.06.00/
-  export DOOSELECTIONSYS=/doosoft/dooselection/ROOT_v6.04.00/
-
-  export PATH=$PATH:$DOOCORESYS/bin:$DOOFITSYS/bin:$DOOSELECTIONSYS/bin
-  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$DOOCORESYS/lib:$DOOFITSYS/lib:$DOOSELECTIONSYS/lib
-}
+alias ls='ls --color'
+alias ll='ls -l'
+alias la='ll -a'
 
 function it2prof() {
   echo -e "\033]50;SetProfile=$1\a"
@@ -34,7 +9,6 @@ function it2prof() {
 function it2proftmux() {
   echo -e "\033Ptmux;\033\033]50;SetProfile=$1\007\033\\"
 }
-
 
 function dark()
 {
@@ -65,7 +39,6 @@ function light()
 }
 
 if [ -n "$BASH_VERSION" ]; then
-  export -f setROOT
   export -f dark
   export -f light
 fi
--- bash/.bash_aliases
+++ bash/.bash_aliases
@@ -1,10 +1,6 @@
+echo "LOADING BASH_ALIASES"
 # User defined aliases (common to zsh)
 if [[ -s "$HOME/.aliases.sh" ]]; then
   source "$HOME/.aliases.sh"
 fi
 
-# Aliases for file listing (when using bash)
-[[ $OSTYPE != "darwin17" ]] && alias ls='ls --color'
-[[ $OSTYPE == "darwin17" ]] && alias ls='ls -G'
-alias ll='ls -lh'
-alias la='ll -a'
